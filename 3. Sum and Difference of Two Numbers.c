00d69763406dde82cacc6e723e1b7b7eeafec0db
hshanto707 <hshanto707@gmail.com>
2022-06-15 06:28:21 +0600

    3. Sum and Difference of Two Numbers

---
+++ 3. Sum and Difference of Two Numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, intSum, intDiff;
+    float x, y, floatSum, floatDiff;
+    
+    scanf("%d %d", &a, &b);
+    scanf("%f %f", &x, &y);
+    
+    intSum = a + b;
+    intDiff = a - b;
+    
+    floatSum = x + y;
+    floatDiff = x - y;
+    
+    printf("%d %d\n", intSum, intDiff);
+    printf("%.1f %.1f", floatSum, floatDiff);
+    
+    return 0;
+}
